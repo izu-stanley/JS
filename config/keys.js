4f841e241714b3ff665cfb9162bdb55ea7a77f14
izu-stanley <izuchukwuuchegbu@gmail.com>
2019-12-17 20:04:59 +0100

    Updates

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://izu:izuchukwu@javascript-tutorials-l4dyl.gcp.mongodb.net/test?retryWrites=true&w=majority'
-}
+  mongoURI:
+    "mongodb+srv://izu:izuchukwu@javascript-tutorials-l4dyl.gcp.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 app.use("/api/users", users);
+
+//Use environment port or post 5000
 port = process.env.PORT || 5000;
 
 app.listen(port, () => {
